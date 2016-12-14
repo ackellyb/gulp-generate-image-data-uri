f08e176a6f746a8f607f7c074db56310884d5ae8
Aaron Kelly-Barker <aaron.kelly.barker@gmail.com>
2016-12-14 18:47:33 -0500

    Option to put to JS file added. Also able to output to txt file. Bug fixed where plugin would only work on one src file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const through = require('through2');
 const DataURI = require('datauri');
+const path = require('path');
+const camelCase = require('camelcase');
 const PluginError = require('gulp-util').PluginError;
 
 const PLUGIN_NAME = 'gulp-generate-image-data-uri';
 
-module.exports = function() {
+module.exports = function(isJS = false) {
 	return through.obj(function(file, encoding, callback) {
 		if (file.isNull()) {
 			callback(null, file);
@@ -13,11 +15,17 @@ module.exports = function() {
 		} else if (file.isBuffer()) {
 			let datauri = new DataURI();
 			datauri.format(file.relative, file.contents);
-			file.contents = new Buffer(datauri.content, encoding);
-			this.push(file);
-		} else {
-			this.emit('error', new PluginError(PLUGIN_NAME, 'Unrecognized filetype. WTF: What the File?'));
-			callback(null, file);
+			const pathData = path.parse(file.path);
+			if (isJS) {
+				const fileName = camelCase(file.relative);
+				file.contents = new Buffer(`const ${fileName} = '${datauri.content}';`, encoding);
+				file.path = path.join(pathData.dir, pathData.name + '.js');
+			} else {
+				file.contents = new Buffer(datauri.content, encoding);
+				file.path = path.join(pathData.dir, pathData.name + '.txt');
+			}
 		}
+		this.push(file);
+		callback();
 	});
 };
